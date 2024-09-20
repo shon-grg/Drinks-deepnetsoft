6da6c05bf0791f333adee53a9c95e76bbcc0f668
Shon George <shongeorge2000@gmail.com>
2024-09-20 19:16:45 +0530

    Deatabase updated

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,37 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
-// PORT
-const port = process.env.PORT || 3000;
 
+//MONGO DB ATLAS PASSWORD CHECKING
+const DB = process.env.DATABASE_MONGOSHELL.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+//MONGO DB CONNECTION STRING
 mongoose
-  .connect(process.env.DATABASE, {
+  //.connect(process.env.DATABASE_LOCAL,{
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
     family: 4,
-  })
-  .then(() => console.log("DB connection successful"))
-  .catch((error) => console.error("DB connection error:", error));
 
-app.listen(port, () => {
-  console.log(`App running on ${port}...`);
+    //     useCreateIndex:true,
+    //     useFindAndModify:false
+  })
+  .then((con) => console.log("DB Connection successful!"));
+
+//PORT & It's ERROR HANDLER
+const port = process.env.PORT;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDIER REJECTION: Shutting down... ");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
