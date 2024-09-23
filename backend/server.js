0418b929f4f2a49ac8622e152e5b1abcac7583f3
Shon George <shongeorge2000@gmail.com>
2024-09-23 10:45:05 +0530

    edit packegg.json

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,29 @@
-//MONGO DB ATLAS PASSWORD CHECKING
-const DB = process.env.DATABASE_MONGOSHELL.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
+// //MONGO DB ATLAS PASSWORD CHECKING
+// const DB = process.env.DATABASE_MONGOSHELL.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSWORD
+// );
 
-//MONGO DB CONNECTION STRING
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    family: 4,
-  })
-  .then((con) => console.log("DB Connection successful!"));
+// //MONGO DB CONNECTION STRING
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     family: 4,
+//   })
+//   .then((con) => console.log("DB Connection successful!"));
 
-//PORT & It's ERROR HANDLER
-const port = process.env.PORT;
-const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+// //PORT & It's ERROR HANDLER
+// const port = process.env.PORT;
+// const server = app.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
 
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDIER REJECTION: Shutting down... ");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on("unhandledRejection", (err) => {
+//   console.log("UNHANDIER REJECTION: Shutting down... ");
+//   console.log(err.name, err.message);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
