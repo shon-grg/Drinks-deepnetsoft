742694c5b834583fd5b9708e22f1165c0baaac0a
Shon George <shongeorge2000@gmail.com>
2024-09-23 11:24:56 +0530

    reedit App.js

--- backend/app.js
+++ backend/app.js
@@ -1,60 +1,3 @@
-// const express = require("express");
-// const morgan = require("morgan");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-
-// const drinksRouter = require("./routes/drinkRoutes");
-// const cocktailsRouter = require("./routes/cocktailsRoutes");
-
-// dotenv.config({ path: "./config.env" });
-
-// const app = express();
-// app.use(cors());
-// // MIDDLEWARES
-// app.use(morgan("dev"));
-
-// app.use(express.json());
-
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString;
-//   next();
-// });
-
-// // COMMON ROUTES
-// app.use("/api/v1/drinks", drinksRouter);
-// app.use("/api/v1/cocktails", cocktailsRouter);
-
-// //MONGO DB ATLAS PASSWORD CHECKING
-// const DB = process.env.DATABASE_MONGOSHELL.replace(
-//   "<PASSWORD>",
-//   process.env.DATABASE_PASSWORD
-// );
-
-// //MONGO DB CONNECTION STRING
-// mongoose
-//   .connect(DB, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     family: 4,
-//   })
-//   .then((con) => console.log("DB Connection successful!"));
-
-// //PORT & It's ERROR HANDLER
-// const port = process.env.PORT || 3000;
-// const server = app.listen(port, () => {
-//   console.log(`App running on port ${port}...`);
-// });
-
-// process.on("unhandledRejection", (err) => {
-//   console.log("UNHANDIER REJECTION: Shutting down... ");
-//   console.log(err.name, err.message);
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
-
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -68,13 +11,13 @@ dotenv.config({ path: "./config.env" });
 
 const app = express();
 app.use(cors());
-
 // MIDDLEWARES
 app.use(morgan("dev"));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
+  req.requestTime = new Date().toISOString;
   next();
 });
 
@@ -88,20 +31,24 @@ const DB = process.env.DATABASE_MONGOSHELL.replace(
   process.env.DATABASE_PASSWORD
 );
 
-// MONGO DB CONNECTION STRING
+//MONGO DB CONNECTION STRING
 mongoose
-  .connect(DB)
-  .then(() => console.log("DB Connection successful!"))
-  .catch((err) => console.log("DB Connection error: ", err.message));
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    family: 4,
+  })
+  .then((con) => console.log("DB Connection successful!"));
 
-// PORT & Its ERROR HANDLER
+//PORT & It's ERROR HANDLER
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
 process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION: Shutting down...");
+  console.log("UNHANDIER REJECTION: Shutting down... ");
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
